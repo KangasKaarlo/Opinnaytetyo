5a0a36369756a75a53949f69b900033a350d46b9
Kaarlo Kangas <kaarlo.kangas@gmail.com>
2023-06-27 15:09:50 +0300

    Add code for CircleBuffer

--- Compressor/Source/CircleBuffer.cpp
+++ Compressor/Source/CircleBuffer.cpp
@@ -9,3 +9,37 @@
 */
 
 #include "CircleBuffer.h"
+
+CircleBuffer::CircleBuffer()
+{
+    buffer = juce::AudioSampleBuffer();
+    writeIndex = 0;
+    readIndex = 0;
+    delay = 0;
+}
+
+CircleBuffer::CircleBuffer(int bufferLength, int delay)
+{
+    buffer = juce::AudioSampleBuffer(1, bufferLength);
+    buffer.clear();
+    writeIndex = delay;
+    readIndex = 0;
+    this->delay = delay;
+}
+
+void CircleBuffer::nextSample()
+{
+    int bufferLength = buffer.getNumSamples();
+    readIndex = ((bufferLength + writeIndex) - delay % bufferLength);
+    writeIndex = (writeIndex + 1) % bufferLength;
+}
+
+float CircleBuffer::getData()
+{
+    return buffer.getSample(0, readIndex);
+}
+
+void CircleBuffer::setData(float data)
+{
+    buffer.setSample(0, writeIndex, data);
+}
--- Compressor/Source/CircleBuffer.h
+++ Compressor/Source/CircleBuffer.h
@@ -9,3 +9,17 @@
 */
 
 #pragma once
+#include <juce_audio_basics/buffers/juce_AudioSampleBuffer.h>
+class CircleBuffer {
+public:
+    CircleBuffer();
+    CircleBuffer(int bufferLength, int delay);
+    void nextSample();
+    float getData();
+    void setData(float data);
+private:
+    juce::AudioSampleBuffer buffer;
+    int readIndex;
+    int writeIndex;
+    int delay;
+};
